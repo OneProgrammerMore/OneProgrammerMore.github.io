6bbea50ee51ebcdf0891ac011b9966ca28a27469
spider <spider@linux.mylinux>
2024-06-20 17:40:28 +0200

    SomeImprovements_StillPreAlpha

--- animations.js
+++ animations.js
@@ -20,42 +20,20 @@ function reveal() {
 }
 
 function openNav(){
-	let width = screen.width;
-	let avWidth = screen.availWidth;
-	if( width <= 600  ||  avWidth  <=600){
-		document.getElementById("nav-menu").style.width = "100%";
-		document.getElementsByClassName("menu-i")[0].style.display = "none";
-		document.getElementsByClassName("close-i")[0].style.display = "flex";
-	}
 
+	document.getElementById("nav-menu").style.width = "100%";
+	document.getElementsByClassName("menu-i")[0].style.display = "none";
+	document.getElementsByClassName("close-i")[0].style.display = "flex";
 }
 
 
 function closeNav(){
-	let width = screen.width;
-	let avWidth = screen.availWidth;
-	if( width <= 600  ||  avWidth  <=600){
-		document.getElementById("nav-menu").style.width = "0px";
-		document.getElementsByClassName("close-i")[0].style.display = "none";
-		document.getElementsByClassName("menu-i")[0].style.display = "flex";
-	}
+
+	document.getElementById("nav-menu").style.width = "0px";
+	document.getElementsByClassName("close-i")[0].style.display = "none";
+	document.getElementsByClassName("menu-i")[0].style.display = "flex";
 	
 }
-
-function hideMenuButton(){
-	let width = screen.width;
-	let avWidth = screen.availWidth;
-	if( width > 600 ){
-		document.getElementsByClassName("close-i")[0].style.display = "none";
-		document.getElementsByClassName("menu-i")[0].style.display = "none";
-		document.getElementById("nav-menu").style.width = "100%";
-	}else{
-		document.getElementById("nav-menu").style.width = "";
-		document.getElementsByClassName("menu-i")[0].style.display = "";
-		document.getElementsByClassName("close-i")[0].style.display = "";
-	}
-}
-
 
 window.onresize = hideMenuButton;
 
